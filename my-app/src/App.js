47b781e39f518da78bf60d661c3d6105842d0a5b
sammy paiz <sammypaiz@sammypaiz-2.local>
2019-01-09 13:31:56 -0800

    state & props:re-render components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,16 +3,32 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons: [
+      {name:'Sam', age: 28},
+      {name:'Glenda', age:39},
+      {name:'Ariana', age:4},
+      {name:'Christian', age: 35},
+      {name:'Wil', age:34}
+    ]
+  }
+
   render() {
-      return(
-          <div className='App'>
-            <h1>Hi, I'm a React App!</h1>
-            <p>This is really working!</p>
-            <Person />
-          </div>
-        );
-    //return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hello World!'));    
+    return(
+      <div className='App'>
+        <h1>Hi, I'm a React App!</h1>
+        <p>This is really working!</p>
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <Person name={this.state.persons[3].name} age={this.state.persons[3].age}>My hobbies: Research!</Person>
+        <Person name={this.state.persons[4].name} age={this.state.persons[4].age} />
+      </div>
+      );
   }
 }
 
 export default App;
+
+
--- my-app/src/Person/Person.js
+++ my-app/src/Person/Person.js
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-const person = () => {
-	return <p>I'm a Person!</p>
-};
+const person = (props) => {
+	return (
+		<div>
+			<p> This is {props.name} and my age is {props.age}</p>
+			<p>{props.children}</p>
+		</div>
+		);
+}
 
 export default person;
+
+
